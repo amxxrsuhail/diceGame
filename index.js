a2f1cfa047d6b562304d4b2a3705c1552e9af061
ameer suhail <kameer@gmail.com>
2022-11-10 00:08:51 +0530

    added session storage in order to run the javascript code after refresh

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-diceRoll();
+// to run the javascript code after refresh
+if (sessionStorage.getItem("is_reloaded")) diceRoll();
+sessionStorage.setItem("is_reloaded", true);
+
 function diceRoll() {
-// first image
+    // first image
     var randomNumber1 = `images/dice${Math.floor(Math.random() * 6) + 1}.png`;
     var image = document.querySelector('.img1');
     image.setAttribute("src", randomNumber1);
-// second image
+    // second image
     var randomNumber2 = `images/dice${Math.floor(Math.random() * 6) + 1}.png`;
     var image = document.querySelector('.img2');
     image.setAttribute("src", randomNumber2);
@@ -12,15 +15,15 @@ function diceRoll() {
     result(randomNumber1, randomNumber2);
 }
 
-function result(firstDice,secondDice){
+function result(firstDice, secondDice) {
     var heading = document.querySelector('h1');
-    if(firstDice===secondDice){
+    if (firstDice === secondDice) {
         heading.innerHTML = "DRAW"
     }
-    else if(firstDice>secondDice){
+    else if (firstDice > secondDice) {
         heading.innerHTML = "Player 1 Wins 🚩"
     }
-    else{
+    else {
         heading.innerHTML = "Player 2 Wins 🚩";
     }
 }
